efdb3273d9795c7da892d88310201fed0307c237
Artur Khelshtein <artur.khelshtein@live.com>
2025-05-10 22:20:49 +0300

    feat: GET

--- index.ts
+++ index.ts
@@ -1,11 +1,31 @@
 const http = require('node:http');
+const { URL: URLParser } = require('url');
+const { v4: uuidV4 } = require('uuid');
 
-const server = http.createServer((_req: any, res: { end: () => void; }) => {
-    console.log('server work')
-  res.end();
+const { PORT = 8000 } = process.env;
+
+interface IUser {
+    id: string;
+    username: string;
+    age: number;
+    hobbies: string[];
+}
+
+const users : IUser[] = [];
+
+const server = http.createServer((req: any, res: any) => {
+    const url = new URLParser(req.url || '', `http://${req.headers.host}`);
+
+    if (req.method === 'GET' && url.pathname === '/api/users') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(users));
+        return;
+    }
+
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
 });
-server.on('clientError', (_err: any, socket: { end: () => void; }) => {
-    console.log('HTTP/1.1 400 Bad Request\r\n\r\n');
-    socket.end();
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-server.listen(8000);
